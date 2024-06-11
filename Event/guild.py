773e4027c32fcbd142aa6767f1eb74a43bb9d395
CHISADIN <tranthanhnam.ccc@gmail.com>
2024-06-11 10:20:30 +0700

    sửa lỗi (2)

--- Event/guild.py
+++ Event/guild.py
@@ -1,7 +1,9 @@
+import logging
+
 import disnake
 from disnake.ext import commands
+
 from utils.ClientUser import ClientUser
-import  logging
 
 logger = logging.getLogger(__name__)
 
@@ -14,17 +16,18 @@ class GuildEntry(commands.Cog):
     async def remove_data(self, guild: disnake.Guild):
 
         print(f"Bị xóa khỏi máy chủ: {guild.name} - [{guild.id}]")
-        data = await self.client.serverdb.get_webhook(guild.id)
+        data = await self.client.serverdb.get_guild_webhook(guild.id)
         
         if data["status"] == "No_Data":
             return
         
-        guild_webhook = data["webhook_uri"]
+        guild_webhook = data
         
         try:
             await self.client.serverdb.remove_server_log(guild.id, guild_webhook)
             await self.client.serverdb.remove_ignored_role_data(guild.id)
             await  self.client.serverdb.remove_language_on_leave(guild.id)
+            await self.client.serverdb.delcache(guild.id)
         except Exception as e:
             logger.error(f"Đã xảy ra lỗi {e}")
             ...
