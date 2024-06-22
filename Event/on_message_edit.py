f27883808eb267341b5aca057751472158562171
Shindou Aris <92295497+ShindouAris@users.noreply.github.com>
2024-06-22 13:06:13 +0000

    .author (2)

--- Event/on_message_edit.py
+++ Event/on_message_edit.py
@@ -15,7 +15,7 @@ class OnMessageEdit(commands.Cog):
     @commands.Cog.listener()
     async def on_message_edit(self, before: disnake.Member, after):
         if not before.guild or before.author.bot: return
-        check = await self.client.serverdb.check_mute(before.roles, before.guild.id)
+        check = await self.client.serverdb.check_mute(before.author.roles, before.guild.id)
         if check == True: return
         if before.content == after.content:
             return #! Ignore if the message is the same
